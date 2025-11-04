fafaf04e0dd805dc4cdb93320fef3944f1cd97ac
Ethan-Choy <ethan.choy@stu.ocsb.ca>
2025-11-04 13:30:12 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Ethan Choy
+ * Created on: Sep 2025
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// variables
+let sprite: game.LedSprite = null
+let sprite1: number = 0
+let turn: number = 0
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    while (sprite1 < 5) {
+        sprite.move(1)
+        sprite1 = + 1
+        turn = + 1
+
+    while (turn > 5) {
+        sprite.turn(Direction.Right, 90)
+        
+    
+    }
+
+})
